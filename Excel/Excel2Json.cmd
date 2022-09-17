45d2bb1faa6e93dbbb21c4f5bba1092768920989
QianHuihuang <490762475@qq.com>
2022-09-17 18:14:12 +0800

    excel

---
+++ Excel/Excel2Json.cmd
@@ -0,0 +1,8 @@
+json-excel\json-excel json Tables\ Data\
+
+@copy Data\Character.txt ..\Client\Data\
+@copy Data\Map.txt ..\Client\Data\
+
+@copy Data\Character.txt ..\Server\GameServer\GameServer\bin\Debug\Data\
+@copy Data\Map.txt ..\Server\GameServer\GameServer\bin\Debug\Data\
+pause
